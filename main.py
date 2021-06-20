13ea9e7037c7d6da52be5623ba32d9ce7ebce85c
negative1skunk@gmail.com <silaslowe@gmail.com>
2021-06-19 21:27:54 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import numpy as np
+from PIL import Image
+
+data = np.zeros((5, 4, 3), dtype=np.uint8)
+data[:] = [255, 255, 0]
+
+data[1:3, 1:3] = [255, 0, 0]
+
+
+img = Image.fromarray(data, 'RGB')
+img.save('canvas.png')
